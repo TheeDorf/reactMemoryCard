eb51352920f672e748c103b4fddb188c3b2395ae
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-29 12:56:52 -0700

    start game idea

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from "react"
 
-class MemoryGame extends Component{
-  constructor(props){
-    super(props);
-    this.state= {
-      score: 0,
-      bestScore: 0,
-      cards:[
-        {id: 1, text: "Card 1" },
-        {id: 2,text: "Card 2"},
-        {id: 3,text: "Card 3"},
-        {id: 4,text: "Card 4"}
-      ],
-      clickedCards: [],
-    };
-    this.shuffleCards = this.shuffleCards.bind(this);
-  }
+function MemoryGame (){
+  const [sequence,setSequence]= useState([]);
+  const [currentInput, setCurrentInput]= useState("")
+  const [inputIndex, setInputIndex]= useState(0)
+  const [score, setScore] = useState(0);
+
+
+
+
+
+
+
+
+  
 }
 
 
+render(){
+  return(
+    <div>
+    </div>
+  );
+}
 
-export default MemoryGame

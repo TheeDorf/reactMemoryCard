7e2c9cdc231e6d029a0634e90ba891b1bb3c1eea
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-30 13:24:49 -0700

    fix an error

--- src/components/Prompt.jsx
+++ src/components/Prompt.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Prompt extends Component{
     render(){
-        const { item }= this,props;
+        const { item }= this.props;
         return(
             <div>
                 <p>Have you seen this letter?</p>

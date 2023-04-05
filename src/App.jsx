6ce86832224251684d2efb87b06aded877ad7421
AJ Dorfman <ajdorfman91@gmail.com>
2023-04-05 15:58:24 -0700

    work on project

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,28 @@ class App extends Component{
       currentScore:0,
       bestScore:0,
     };
+
+    this.handleScore = this.handleScore.bind(this);
   }
+
+handleScore(increment){
+  if (increment){
+    this.setState({
+      currentScore: this.state.currentScore +1,
+    });
+  } else{
+    this.setState({
+      currentScore: 0,
+    });
+  }
+}
 
 render(){
   const {currentScore,bestScore} = this.state;
   return(
     <div className="App">
       <Header currentScore={currentScore} bestScore={bestScore} />
-      <Game />
+      <Game handleScore={this.handleScore} />
 
     </div>
   );
@@ -26,4 +40,4 @@ render(){
 }
 
 
-export default App
+export default App;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react"
-
+import Prompt from "./Prompt"
 const items = [];
 
-for (let i =0; i<10; i++){
+for (let i =65; i<80; i++){
     items.push({
-        id:i,
+        id: i,
         content: String.fromCharCode(i),
-        displayed:false,
+        displayed: false,
 
     });
 }
@@ -17,20 +17,48 @@ class Game extends Component{
         this.state = {
             items,
         };
+
+        this.handleAnswer = this.handleAnswer.bind(this);
     }
+    
+    handleAnswer(event, item, index, answer){
+        // compare item.display with answer
+        // increment or reset score
+        this.props.handleScore(item.displayed === answer);
+
+
+        // change the item that is displayed 
+        if(!item.displayed){
+            const items = this.state.items.map(item) => { 
+                if(item.displayed){
+                    return item;
+
+                } else {
+                    item.displayed = true;
+                    return item;
+                }
+            };
+            this.setState({ items });
+        }
+    }
+
+
         randomItemIndex(){
             let rand = Math.floor(Math.random() * items.length);
             return rand;
           
         }
+
     
     render(){
-        const {items} = this.state;
-        const item = items[this.randomItemIndex()].content
+        const { items } = this.state;
+        const index = this.randomItemIndex()
+        const item = items[index];
+
         return(
             <main className="d-flex justify-content-around align-items-center">
-                <p>{items[this.randomItemIndex()].content}</p>
-               <Prompt />
+                <p id={index}>Random Letter is " {item.content} "</p>
+             <Prompt  item={item} index={index} handleAnswer={this.handleAnswer} />
             </main>
         );
     }
